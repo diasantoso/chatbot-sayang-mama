b13e7879e015f70b6e8f62369a4412850a676b69
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-12 16:19:12 +0700

    Fixing Reminder Cron Job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Command\SendReminder',
+        'App\Console\Commands\SendReminder',
     ];
 
     /**

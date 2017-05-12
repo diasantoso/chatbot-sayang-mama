36cee54e85ec9ed19186e522e5dc5f628646385d
Unknown <kusumayudha32@gmail.com>
2017-05-12 10:46:05 +0700

    Route User dan Prodi

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('user/landingpage');
 });
+
+
+
+
+//user
+Route::get('User', 'UserController@index')->name('user.index');
+Route::post('User.store', 'UserController@store')->name('user.store');
+Route::patch('User.update', 'PegawaiController@update')->name('user.update');
+Route::get('User.delete/{id}', 'UserController@destroy')->name('user.delete');
+
+
+//prodi
+
+Route::get('Prodi', 'ProdiController@index')->name('prodi.index');
+Route::post('Prodi.store', 'ProdiController@store')->name('prodi.store');
+Route::patch('Prodi.update', 'ProdiController@update')->name('prodi.update');
+Route::get('Prodi.delete/{id}', 'ProdiController@destroy')->name('prodi.delete');

98c340ee3024994c02c28a5edfa3672cc46a6fd1
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-12 08:48:04 +0700

    Tambah Database
    - jadwal_tambahan

--- chatbot.sql
+++ chatbot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2017 at 08:11 AM
+-- Generation Time: May 12, 2017 at 08:45 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -63,11 +63,50 @@ CREATE TABLE `jadwal` (
   `user_id` int(11) NOT NULL DEFAULT '0',
   `sesi_prodi_id` int(11) NOT NULL DEFAULT '0',
   `sesi_prodi_id_selesai` int(11) DEFAULT '0',
-  `makul` varchar(250) NOT NULL,
+  `makul_id` int(11) NOT NULL,
   `keyword` varchar(250) NOT NULL DEFAULT '0',
   `kelas` varchar(50) NOT NULL,
   `ruangan` varchar(250) NOT NULL,
   `pengingat` time DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_tambahan`
+--
+
+CREATE TABLE `jadwal_tambahan` (
+  `id` int(11) NOT NULL,
+  `makul_id` int(11) NOT NULL DEFAULT '0',
+  `nama` varchar(254) DEFAULT NULL,
+  `deskripsi` varchar(254) DEFAULT NULL,
+  `waktu_mulai` datetime DEFAULT NULL,
+  `waktu_selesai` datetime DEFAULT NULL,
+  `type` enum('Kuis','Tugas') DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `makul`
+--
+
+CREATE TABLE `makul` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
@@ -97,6 +136,25 @@ CREATE TABLE `prodi` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `prodi_sesi`
+--
+
+CREATE TABLE `prodi_sesi` (
+  `id` int(5) NOT NULL,
+  `prodi_id` int(5) NOT NULL DEFAULT '0',
+  `sesi_id` int(5) NOT NULL DEFAULT '0',
+  `waktu` time NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sesi`
 --
 
@@ -115,35 +173,14 @@ CREATE TABLE `sesi` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sesi_prodi`
---
-
-CREATE TABLE `sesi_prodi` (
-  `id` int(5) NOT NULL,
-  `prodi_id` int(5) NOT NULL DEFAULT '0',
-  `sesi_id` int(5) NOT NULL DEFAULT '0',
-  `waktu` time NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `deleted_at` datetime DEFAULT NULL,
-  `created_by` int(11) NOT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `deleted_by` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `chat_log_id` int(11) DEFAULT NULL,
   `chat_log_line_id` int(11) DEFAULT NULL,
-  `chat_log_fb_id` int(11) DEFAULT NULL,
-  `telegram_username` varchar(250) DEFAULT NULL,
   `chat_id` int(11) DEFAULT NULL,
+  `jangka_reminder` int(11) DEFAULT NULL,
   `fullname` varchar(250) NOT NULL,
   `npm` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
@@ -167,8 +204,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `chat_log_id`, `chat_log_line_id`, `chat_log_fb_id`, `telegram_username`, `chat_id`, `fullname`, `npm`, `email`, `role`, `password`, `lupa_pass`, `status`, `image`, `prodi_id`, `fakultas_id`, `registerdate`, `created_at`, `updated_at`, `deleted_at`, `updated_by`, `deleted_by`, `remember_token`) VALUES
-(1, 1, 1, NULL, 'vincentiusdito', NULL, 'Dito Raharjo', '140707962', 'ditoparker@gmail.com', 'Admin', '$2y$10$iVru9d3HV71wgsRfUeEWHuyn3nG/FD691pjnT/MgIMU8IW/dAPe3y', NULL, '1', 'Dito Raharjo_2017-03-20.jpg', 13, 6, '2017-02-15', '2017-02-24 11:35:39', '2017-03-19 18:25:33', NULL, NULL, NULL, '8DqDPhimt5BuMvyn3K6vctZ9o5RD3X6XAIW6qNfebGUIJniVnbS5Fo20lmmh');
+INSERT INTO `users` (`id`, `chat_log_line_id`, `chat_id`, `jangka_reminder`, `fullname`, `npm`, `email`, `role`, `password`, `lupa_pass`, `status`, `image`, `prodi_id`, `fakultas_id`, `registerdate`, `created_at`, `updated_at`, `deleted_at`, `updated_by`, `deleted_by`, `remember_token`) VALUES
+(1, 1, NULL, NULL, 'Dito Raharjo', '140707962', 'ditoparker@gmail.com', 'Admin', '$2y$10$iVru9d3HV71wgsRfUeEWHuyn3nG/FD691pjnT/MgIMU8IW/dAPe3y', NULL, '1', 'Dito Raharjo_2017-03-20.jpg', 13, 6, '2017-02-15', '2017-02-24 11:35:39', '2017-03-19 18:25:33', NULL, NULL, NULL, '8DqDPhimt5BuMvyn3K6vctZ9o5RD3X6XAIW6qNfebGUIJniVnbS5Fo20lmmh');
 
 --
 -- Indexes for dumped tables
@@ -193,21 +230,33 @@ ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `jadwal_tambahan`
+--
+ALTER TABLE `jadwal_tambahan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `makul`
+--
+ALTER TABLE `makul`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `prodi`
 --
 ALTER TABLE `prodi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `sesi`
+-- Indexes for table `prodi_sesi`
 --
-ALTER TABLE `sesi`
+ALTER TABLE `prodi_sesi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `sesi_prodi`
+-- Indexes for table `sesi`
 --
-ALTER TABLE `sesi_prodi`
+ALTER TABLE `sesi`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -237,19 +286,29 @@ ALTER TABLE `fakultas`
 ALTER TABLE `jadwal`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `jadwal_tambahan`
+--
+ALTER TABLE `jadwal_tambahan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `makul`
+--
+ALTER TABLE `makul`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `prodi_sesi`
+--
+ALTER TABLE `prodi_sesi`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `sesi`
 --
 ALTER TABLE `sesi`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `sesi_prodi`
---
-ALTER TABLE `sesi_prodi`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`

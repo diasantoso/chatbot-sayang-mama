ad46e34d8c39e28766d000fd73d4485f88240bf6
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-12 15:16:13 +0700

    Adding Latest Database
    - change some field to "Allow NULL"

--- chatbot.sql
+++ chatbot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2017 at 08:45 AM
+-- Generation Time: May 12, 2017 at 03:15 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -84,12 +84,14 @@ CREATE TABLE `jadwal` (
 
 CREATE TABLE `jadwal_tambahan` (
   `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
   `makul_id` int(11) NOT NULL DEFAULT '0',
-  `nama` varchar(254) DEFAULT NULL,
+  `nama` varchar(254) NOT NULL,
   `deskripsi` varchar(254) DEFAULT NULL,
-  `waktu_mulai` datetime DEFAULT NULL,
-  `waktu_selesai` datetime DEFAULT NULL,
-  `type` enum('Kuis','Tugas') DEFAULT NULL,
+  `waktu_mulai` datetime NOT NULL,
+  `waktu_selesai` datetime NOT NULL,
+  `type` enum('Kuis','Tugas') NOT NULL,
+  `keyword` varchar(250) DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
@@ -143,7 +145,6 @@ CREATE TABLE `prodi_sesi` (
   `id` int(5) NOT NULL,
   `prodi_id` int(5) NOT NULL DEFAULT '0',
   `sesi_id` int(5) NOT NULL DEFAULT '0',
-  `waktu` time NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
@@ -162,6 +163,7 @@ CREATE TABLE `sesi` (
   `id` int(5) NOT NULL,
   `hari` varchar(10) NOT NULL,
   `sesi` int(5) NOT NULL,
+  `waktu` time NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
@@ -182,15 +184,14 @@ CREATE TABLE `users` (
   `chat_id` int(11) DEFAULT NULL,
   `jangka_reminder` int(11) DEFAULT NULL,
   `fullname` varchar(250) NOT NULL,
-  `npm` varchar(250) NOT NULL,
+  `npm` varchar(250) DEFAULT NULL,
   `email` varchar(250) NOT NULL,
-  `role` varchar(250) NOT NULL,
+  `role` enum('Mahasiswa','Administrator') NOT NULL,
   `password` varchar(250) NOT NULL,
   `lupa_pass` varchar(250) DEFAULT NULL,
   `status` enum('0','1') NOT NULL DEFAULT '1',
   `image` varchar(250) DEFAULT NULL,
-  `prodi_id` int(5) NOT NULL DEFAULT '0',
-  `fakultas_id` int(5) NOT NULL DEFAULT '0',
+  `prodi_id` int(5) DEFAULT '0',
   `registerdate` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -204,8 +205,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `chat_log_line_id`, `chat_id`, `jangka_reminder`, `fullname`, `npm`, `email`, `role`, `password`, `lupa_pass`, `status`, `image`, `prodi_id`, `fakultas_id`, `registerdate`, `created_at`, `updated_at`, `deleted_at`, `updated_by`, `deleted_by`, `remember_token`) VALUES
-(1, 1, NULL, NULL, 'Dito Raharjo', '140707962', 'ditoparker@gmail.com', 'Admin', '$2y$10$iVru9d3HV71wgsRfUeEWHuyn3nG/FD691pjnT/MgIMU8IW/dAPe3y', NULL, '1', 'Dito Raharjo_2017-03-20.jpg', 13, 6, '2017-02-15', '2017-02-24 11:35:39', '2017-03-19 18:25:33', NULL, NULL, NULL, '8DqDPhimt5BuMvyn3K6vctZ9o5RD3X6XAIW6qNfebGUIJniVnbS5Fo20lmmh');
+INSERT INTO `users` (`id`, `chat_log_line_id`, `chat_id`, `jangka_reminder`, `fullname`, `npm`, `email`, `role`, `password`, `lupa_pass`, `status`, `image`, `prodi_id`, `registerdate`, `created_at`, `updated_at`, `deleted_at`, `updated_by`, `deleted_by`, `remember_token`) VALUES
+(1, 1, NULL, NULL, 'Dito Raharjo', '140707962', 'ditoparker@gmail.com', '', '$2y$10$iVru9d3HV71wgsRfUeEWHuyn3nG/FD691pjnT/MgIMU8IW/dAPe3y', NULL, '1', 'Dito Raharjo_2017-03-20.jpg', 13, '2017-02-15', '2017-02-24 11:35:39', '2017-03-19 18:25:33', NULL, NULL, NULL, '8DqDPhimt5BuMvyn3K6vctZ9o5RD3X6XAIW6qNfebGUIJniVnbS5Fo20lmmh');
 
 --
 -- Indexes for dumped tables
